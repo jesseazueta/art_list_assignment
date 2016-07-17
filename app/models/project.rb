cddf6da0526ce704430492da8fe76b3585469b89
Jesus Barrenechea <jesseazueta@gmail.com>
2016-07-16 23:03:34 -0400

    Added end date

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,7 @@
 class Project < ApplicationRecord
   validates :description, :client, presence: true
   validates :price_in_cents, numericality: {only_integer: true, greater_than: 0}
-  validates :start_date, presence: true
+  validates :start_date, :end_date, presence: true
   def formatted_price
     price_in_dollars = price_in_cents.to_f / 100
     format("% .2f", price_in_dollars)
@@ -9,7 +9,10 @@ class Project < ApplicationRecord
 
   def date_start
     start_date.strftime("%m/%d/%y")
-  
+  end
+
+  def date_end
+    end_date.strftime("%m/%d/%y")
   end
 
 

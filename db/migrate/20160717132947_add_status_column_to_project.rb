1ce0cc0be149a6acd04cf6b6f806c2a7216d7cfe
Jesus Barrenechea <jesseazueta@gmail.com>
2016-07-17 20:31:08 -0400

    Css styles begin

--- db/migrate/20160717132947_add_status_column_to_project.rb
+++ db/migrate/20160717132947_add_status_column_to_project.rb
@@ -2,3 +2,4 @@ class AddStatusColumnToProject < ActiveRecord::Migration[5.0]
   def change
     add_column :projects, :status, :boolean, :default => false
   end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160717132947) do
+ActiveRecord::Schema.define(version: 20160717200654) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,15 @@ ActiveRecord::Schema.define(version: 20160717132947) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["project_id"], name: "index_reviews_on_project_id", using: :btree
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
   add_foreign_key "reviews", "projects"
